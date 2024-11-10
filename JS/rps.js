99a007bb4c426a7093c9ded11fae37162d20d345
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-11-10 18:44:13 -0500

    Update rps.js
    Optimizing the logic by putting it in the JS file

--- JS/rps.js
+++ JS/rps.js
@@ -1,3 +1,21 @@
+function play(userChoice) {
 
-let computer = '';
-let result = '';
+    let computer = '';
+    let result = '';
+
+
+const num = Math.random();
+
+if(num >= 0 && num < 1/3) {
+    computer = 'rock';
+ }
+
+ else if (num >= 1/3 && num < 2/3) {
+    computer = 'paper';
+ }
+
+ else {
+    computer = 'scisors';
+ }
+
+}
