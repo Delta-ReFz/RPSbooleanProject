7bfd113f19dc6b20a274ddf37a23590c34dd0cef
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-11-10 21:31:28 -0500

    Added a counter
    Added a counter for the amounts of wins, losses & ties

--- JS/rps.js
+++ JS/rps.js
@@ -1,3 +1,12 @@
+const score = {
+
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+console.log(score);
+
 function play(userChoice) {
 
     let computer = '';
@@ -18,9 +27,6 @@ if(num >= 0 && num < 1/3) {
     computer = 'scissors';
  }
 
-
- console.log('Computer choice: ', computer);
-
  if(computer === userChoice) {
  
     result = 'Thats a Tie';
@@ -39,5 +45,18 @@ else {
     result = 'You Lose';
 }
 
-alert('The computer chose ' + computer + '\n' + result);
+if(result === 'You Win') {
+
+    score.wins += 1;
+} else if(result === 'You Lose') {
+
+    score.losses += 1;
+} else if(result === 'Thats a Tie') {
+
+    score.ties += 1;
 }
+
+alert('You picked ' + userChoice +' The computer picked ' + computer + '\n' + result + '\nWins:  ' + 
+    score.wins + ' Losses: ' + score.losses + ' Ties: ' + score.ties);
+}
+
